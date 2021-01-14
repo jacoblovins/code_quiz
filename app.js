8b4099e093d84248fbf97fa2a5b28d797fe6b796
jacoblovins <lovins.jacob@yahoo.com>
2021-01-14 09:32:12 -0500

    deleted unnecessary libraries and moved questions to new file

--- app.js
+++ app.js
@@ -1,74 +1,14 @@
 const container = document.querySelector(".container");
 const timer = document.querySelector("#timer");
 const ifCorrect = document.querySelector("#ifCorrect");
+const startBtn = document.querySelector('#startBtn')
 let q = 0;
 let count = 60;
-
-// questions and answers
-const questionArr = [
-    {
-        question: "What is the HTML tag under which one can write the JavaScript code?",
-        answers: [
-            "1. <javascript>",
-            "2. <scripted>",
-            "3. <script>",
-            "4. <js>"
-        ],
-        correctAnswer: "3. <script>"
-    },
-    {
-        question: "Which of the following is the correct syntax to display an alert box using JavaScript?",
-        answers: [
-            "1. alertbox(“someText”)",
-            "2. msg(“someText”)",
-            "3. msgbox(“someText”)",
-            "4. alert(“someText”)"
-        ],
-        correctAnswer: "4. alert(“someText”)"
-    },
-    {
-        question: "The external JavaScript file must contain <script> tag. True or False?",
-        answers: [
-            "1. True",
-            "2. False"
-        ],
-        correctAnswer: "2. False"
-    },
-    {
-        question: "Who invented JavaScript?",
-        answers: [
-            "1. Brendan Eich",
-            "2. Douglas Crockford",
-            "3. Sheryl Sandberg"
-        ],
-        correctAnswer: "1. Brendan Eich"
-    },
-    {
-        question: "Which of the following is not a reserved word in JavaScript?",
-        answers: [
-            "1. interface",
-            "2. program",
-            "3. throws",
-            "4. short"
-        ],
-        correctAnswer: "2. program"
-    },
-    {
-        question: "What is the JavaScript syntax for printing values in Console?",
-        answers: [
-            "1. print('')",
-            "2. console.log('')",
-            "3. console.print('')",
-            "4. print.console('')"
-        ],
-        correctAnswer: "2. console.log('')"
-    }
-];
 
 // Start screen function
 (function startScreen() {
 
-    startBtn.addEventListener("click", function () {
+    startBtn.addEventListener("click", () => {
         startTimer();
         quizQuestions();
     });
@@ -76,7 +16,7 @@ const questionArr = [
 
 // start the countdown
 const startTimer = () => {
-    var interval = setInterval(function () {
+    const interval = setInterval(function () {
         count--;
         // check if time runs out
         if (count <= 0) {
---
+++ questionsArr.js
@@ -0,0 +1,60 @@
+// questions and answers
+const questionArr = [
+    {
+        question: "What is the HTML tag under which one can write the JavaScript code?",
+        answers: [
+            "1. <javascript>",
+            "2. <scripted>",
+            "3. <script>",
+            "4. <js>"
+        ],
+        correctAnswer: "3. <script>"
+    },
+    {
+        question: "Which of the following is the correct syntax to display an alert box using JavaScript?",
+        answers: [
+            "1. alertbox(“someText”)",
+            "2. msg(“someText”)",
+            "3. msgbox(“someText”)",
+            "4. alert(“someText”)"
+        ],
+        correctAnswer: "4. alert(“someText”)"
+    },
+    {
+        question: "The external JavaScript file must contain <script> tag. True or False?",
+        answers: [
+            "1. True",
+            "2. False"
+        ],
+        correctAnswer: "2. False"
+    },
+    {
+        question: "Who invented JavaScript?",
+        answers: [
+            "1. Brendan Eich",
+            "2. Douglas Crockford",
+            "3. Sheryl Sandberg"
+        ],
+        correctAnswer: "1. Brendan Eich"
+    },
+    {
+        question: "Which of the following is not a reserved word in JavaScript?",
+        answers: [
+            "1. interface",
+            "2. program",
+            "3. throws",
+            "4. short"
+        ],
+        correctAnswer: "2. program"
+    },
+    {
+        question: "What is the JavaScript syntax for printing values in Console?",
+        answers: [
+            "1. print('')",
+            "2. console.log('')",
+            "3. console.print('')",
+            "4. print.console('')"
+        ],
+        correctAnswer: "2. console.log('')"
+    }
+];
